4355ffbcbb637fa93f324af6594683ccdabf5974
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2022-12-05 21:02:47 -0300

    Add data, schema and queries files

---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE animals (
+  id BIGSERIAL NOT NULL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  date_of_birth DATE NOT NULL,
+  escape_attempts INTEGER,
+  neutered BOOLEAN NOT NULL,
+  weight_kg DOUBLE PRECISION NOT NULL
+);
