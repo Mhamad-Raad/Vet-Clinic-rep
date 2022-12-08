ed4fc9ac66c2ec8c8232d106100ccc021540ac6b
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2022-12-08 18:31:29 -0300

    Animals table in data file

--- data.sql
+++ data.sql
@@ -9,3 +9,19 @@ VALUES
 ('Gabumon', DATE '2018 11 15', 8, true, 2),
 ('Pikachu', DATE '2021 01 07', 15.04, false, 1),
 ('Devimon', DATE '2017 05 12', 11, true, 5);
+
+/* Tuesday */
+INSERT INTO animals (
+    name, 
+    date_of_birth, 
+    weight_kg, 
+    neutered,
+    escape_attempts) 
+VALUES 
+('Charmander', DATE '2020 02 08', 11, false, 0),
+('Plantmon', DATE '2021 05 05', 5.7, true, 2),
+('Squirtle', DATE '1993 04 17', 12.13, false, 3),
+('Angemon', DATE '2005 06 12', 45, true, 1),
+('Boarmon', DATE '2005 06 07', 20.4, true, 7),
+('Blossom', DATE '1998 10 13', 17, true, 3),
+('Ditto', DATE '2022 05 14', 22, true, 4);
