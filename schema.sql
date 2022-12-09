5aa0eba8869a679ff419ea9f6e1af445dda1ffc3
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2022-12-09 17:09:19 -0300

    Schema file done

--- schema.sql
+++ schema.sql
@@ -28,3 +28,20 @@ CREATE TABLE species (
 alter table animals drop column species; /*DELETE COLUMN species*/
 alter table animals add column species_id INT NULL REFERENCES species(id) ON DELETE CASCADE; /*ADD COLUMN species_id, which is a foreign key referencing species table*/
 alter table animals add column owner_id INT NULL REFERENCES owners(id) ON DELETE CASCADE; /*ADD COLUMN owner_id, which is a foreign key referencing owners table*/
+
+/* thursday */
+CREATE TABLE vets (
+  id BIGSERIAL NOT NULL PRIMARY KEY,
+  name varchar(100) NOT NULL,
+  age INT NOT NULL,
+  date_of_graduation DATE NOT NULL
+);
+CREATE TABLE specializations (
+  species_id INT NULL REFERENCES species(id) ON DELETE CASCADE,
+  vet_id INT NULL REFERENCES vets(id) ON DELETE CASCADE
+);
+CREATE TABLE visits (
+  animal_id INT NULL REFERENCES animals(id) ON DELETE CASCADE,
+  vet_id INT NULL REFERENCES vets(id) ON DELETE CASCADE,
+  date_of_visit DATE NOT NULL
+);
